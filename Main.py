0e65d9867cb35475c28dd569faa93752862dfff6
LetMeFly666 <814114971@qq.com>
2022-09-29 15:31:39 +0800

    Try, and I assume that it must be wrong

--- Main.py
+++ Main.py
@@ -2,8 +2,12 @@
 Author: LetMeFly
 Date: 2022-09-29 15:16:51
 LastEditors: LetMeFly
-LastEditTime: 2022-09-29 15:19:03
+LastEditTime: 2022-09-29 15:30:14
 '''
+import os
+import time
+
+
 with open("README.md", "r", encoding="utf-8") as f:
     content = f.read()
 
@@ -11,3 +15,13 @@ content += "\n\n66666666666666666\n\n"
 
 with open("README.md", "w", encoding="utf-8") as f:
     f.write(content)
+
+
+def execute1commandAfterEcho(command):
+    print(command)
+    os.system(command)
+
+
+execute1commandAfterEcho("git add .")
+execute1commandAfterEcho("git commit -m \"Updated on {}\"".format(time.strftime("%Y-%m-%d")))
+execute1commandAfterEcho("git push")
